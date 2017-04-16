bef0671ed4581f85143c1a04fc84c0410d360021
Trung Pham <phambaotrung@gmail.com>
2017-04-16 13:26:49 -0700

    Fix build

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 from conan.packager import ConanMultiPackager
 
+
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add_common_builds(shared_option_name="libsodium:shared", pure_c=True)
     builder.run()
